013cfcf566f9a00df383b4460b7a216e725f6a26
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-06-13 11:09:16 -0400

    fixed the necesary specifications

--- API/country.py
+++ API/country.py
@@ -1,17 +1,10 @@
 from flask import Blueprint, request, jsonify, abort, Blueprint
 from country import Country
+from city import City
 from Persistence.DataManager import DataManager
 
 country_bp = Blueprint('country', __name__)
 data_manager = DataManager()
-
-# Ruta para crear un nuevo país
-@country_bp.route('/', methods=['POST'])
-def create_country():
-    data = request.get_json()
-    country = Country(name=data['name'], area_code=data['area_code'])
-    data_manager.save(country)
-    return jsonify({'id': str(country.id)}), 201
 
 # Ruta para obtener la lista de todos los países
 @country_bp.route('/', methods=['GET'])
@@ -27,24 +20,11 @@ def get_country(country_id):
         return jsonify({'error': 'Country not found'}), 404
     return jsonify(country.__dict__)
 
-# Ruta para actualizar la información de un país existente
-@country_bp.route('/<country_id>', methods=['PUT'])
-def update_country(country_id):
-    data = request.get_json()
+@country_bp.route('/<country_id>/cities', methods=['GET'])
+def get_cities_by_country(country_id):
     country = data_manager.get(country_id, Country)
     if country is None:
         return jsonify({'error': 'Country not found'}), 404
-    for key, value in data.items():
-        setattr(country, key, value)
-    country.save()
-    data_manager.update(country)
-    return jsonify({'id': str(country.id)}), 200
 
-# Ruta para eliminar un país específico por su ID
-@country_bp.route('/<country_id>', methods=['DELETE'])
-def delete_country(country_id):
-    country = data_manager.get(country_id, Country)
-    if country is None:
-        return jsonify({'error': 'Country not found'}), 404
-    data_manager.delete(country_id, Country)
-    return '', 204
+    cities = [obj.__dict for obj in data_manager.storage.objects.values() if isinstance(obj, City) and obj.country == country]
+    return jsonify(cities)
